f0aa9cb1682525f0d05ecd9971e4e6f4a0b1ec5a
GhostFantik <ghostfantik@gmail.com>
2021-06-13 16:22:46 +0300

    fix copy-cut-paste operation. Try-except was added

--- work/scripts/right_menu.py
+++ work/scripts/right_menu.py
@@ -29,55 +29,64 @@ class RightMenu:
 
     def paste(self, *args, **kwargs):
         """Paste function."""
-        flag = False
-        if flag:
-            num_index = 0
-            while num_index < len(self.buffer_selected):
-                self.text_field.insert(self.index, self.buffer_selected[num_index])
-                for tag in self.buffer_tags[num_index]:
-                    self.text_field.tag_add(tag, self.index)
-                self.index = self.text_field.index(f'{self.index}+1c')
-                num_index += 1
-        else:
-            index = self.text_field.index(tk.INSERT)
-            self.text_field.insert(index, self.buffer_selected)
-            num_index = 0
-            while self.text_field.compare(index, '<', tk.INSERT):
+        try:
+            flag = False
+            if flag:
+                num_index = 0
+                while num_index < len(self.buffer_selected):
+                    self.text_field.insert(self.index, self.buffer_selected[num_index])
+                    for tag in self.buffer_tags[num_index]:
+                        self.text_field.tag_add(tag, self.index)
+                    self.index = self.text_field.index(f'{self.index}+1c')
+                    num_index += 1
+            else:
+                index = self.text_field.index(tk.INSERT)
+                self.text_field.insert(index, self.buffer_selected)
+                num_index = 0
+                while self.text_field.compare(index, '<', tk.INSERT):
 
-                # Remove all tags
-                flag = True
-                while flag:
-                    tags = self.text_field.tag_names(index)
-                    tag = None
-                    for i in range(len(tags)):
-                        if (tags[i].find('_') != -1 and tags[i].find('#') != -1) or (tags[i].find('.') != -1):
-                            tag = tags[i]
-                            break
-                    if tag:
-                        self.text_field.tag_remove(tag, index)
-                    else:
-                        flag = False
+                    # Remove all tags
+                    flag = True
+                    while flag:
+                        tags = self.text_field.tag_names(index)
+                        tag = None
+                        for i in range(len(tags)):
+                            if (tags[i].find('_') != -1 and tags[i].find('#') != -1) or (tags[i].find('.') != -1):
+                                tag = tags[i]
+                                break
+                        if tag:
+                            self.text_field.tag_remove(tag, index)
+                        else:
+                            flag = False
 
-                for tag in self.buffer_tags[num_index]:
-                    self.text_field.tag_add(tag, index)
-                index = self.text_field.index(f'{index}+1c')
-                num_index += 1
+                    for tag in self.buffer_tags[num_index]:
+                        self.text_field.tag_add(tag, index)
+                    index = self.text_field.index(f'{index}+1c')
+                    num_index += 1
+        except tk.TclError:
+            pass
 
     def copy(self, *args, **kwargs):
         """Copy function."""
-        self.buffer_tags = []
-        self.buffer_selected = self.text_field.selection_get()
-        index = self.text_field.index(f"{tk.SEL_FIRST}")
-        while self.text_field.compare(index, '<', tk.SEL_LAST):
-            self.buffer_tags.append(self.text_field.tag_names(f'{index}'))
-            index = self.text_field.index(f"{index}+1c")
+        try:
+            self.buffer_tags = []
+            self.buffer_selected = self.text_field.selection_get()
+            index = self.text_field.index(f"{tk.SEL_FIRST}")
+            while self.text_field.compare(index, '<', tk.SEL_LAST):
+                self.buffer_tags.append(self.text_field.tag_names(f'{index}'))
+                index = self.text_field.index(f"{index}+1c")
+        except tk.TclError:
+            pass
 
     def cut(self, *args, **kwargs):
         """Cut function."""
-        self.buffer_tags = []
-        self.buffer_selected = self.text_field.selection_get()
-        index = self.text_field.index(f"{tk.SEL_FIRST}")
-        while self.text_field.compare(index, '<', tk.SEL_LAST):
-            self.buffer_tags.append(self.text_field.tag_names(f'{index}'))
-            index = self.text_field.index(f"{index}+1c")
-        self.text_field.delete(tk.SEL_FIRST, tk.SEL_LAST)
+        try:
+            self.buffer_tags = []
+            self.buffer_selected = self.text_field.selection_get()
+            index = self.text_field.index(f"{tk.SEL_FIRST}")
+            while self.text_field.compare(index, '<', tk.SEL_LAST):
+                self.buffer_tags.append(self.text_field.tag_names(f'{index}'))
+                index = self.text_field.index(f"{index}+1c")
+            self.text_field.delete(tk.SEL_FIRST, tk.SEL_LAST)
+        except tk.TclError:
+            pass
